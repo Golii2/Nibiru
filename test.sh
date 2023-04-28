4a6ea4e60672631f673a358fbe2269066d58aa73
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-28 17:49:18 +0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,15 +2,11 @@
 #read -p "Введите значение переменной NIBIRU_MONIKER: " NIBIRU_MONIKER
 #echo "Переменная NIBIRU_MONIKER равна: $NIBIRU_MONIKER"
 
-echo "Введите значение переменной:"
-read NIBIRU_MONIKER
+read -p "Введите значение переменной: " NIBIRU_MONIKER
 echo "Вы ввели: $NIBIRU_MONIKER"
-export NIBIRU_MONIKER=NIBIRU_MONIKER
 
 #read -p "Введите значение переменной NIBIRU_WALLET: " NIBIRU_WALLET
 #echo "Переменная NIBIRU_WALLET равна: $NIBIRU_WALLET"
 
-echo "Введите значение переменной:"
-read NIBIRU_WALLET
+read -p "Введите значение переменной: " NIBIRU_WALLET
 echo "Вы ввели: $NIBIRU_WALLET"
-export NIBIRU_WALLET=NIBIRU_WALLET
