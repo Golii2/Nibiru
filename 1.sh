a37f4e161b580e7f35036aee1a273e409b244b81
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-28 19:48:26 +0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-read -p "Введите значение для NIBIRU_MONIKER: " NIBIRU_MONIKER
-read -p "Введите значение для NIBIRU_WALLET: " NIBIRU_WALLET
+if [[ -z "$NIBIRU_MONIKER" ]]; then
+  read -p "Enter your NIBIRU_MONIKER name : " _NIBIRU_MONIKER
+  export NIBIRU_MONIKER=$_NIBIRU_MONIKER
+fi
 
-export NIBIRU_MONIKER=$NIBIRU_MONIKER
-export NIBIRU_WALLET=$NIBIRU_WALLET
-
-echo "Значения успешно присвоены:"
-echo "NIBIRU_MONIKER=${NIBIRU_MONIKER}"
-echo "NIBIRU_WALLET=${NIBIRU_WALLET}"
+echo 'export NIBIRU_MONIKER='\"$NIBIRU_MONIKER\" >> $HOME/.bash_profile
+echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
+source $HOME/.bash_profile
