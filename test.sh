45ac24cb42267fdac8fc61e55c40de3638a08a06
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-28 17:55:25 +0400

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#read -p "Введите значение переменной NIBIRU_MONIKER: " NIBIRU_MONIKER
-#echo "Переменная NIBIRU_MONIKER равна: $NIBIRU_MONIKER"
-
-read -p "Введите значение переменной: " NIBIRU_MONIKER
-echo "Вы ввели: $NIBIRU_MONIKER"
-
-read -p "Введите значение переменной: " NIBIRU_WALLET
-echo "Вы ввели: $NIBIRU_WALLET"
