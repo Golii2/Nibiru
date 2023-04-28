9b4ff814781cd2a9a3c2ed41654e565cd0172052
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-28 18:10:58 +0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-read -p "Введите значение переменной: " NIBIRU_MONIKER
+read -p "Введите значение переменной NIBIRU_MONIKER: " NIBIRU_MONIKER
 if [[ -z "$NIBIRU_MONIKER" ]]; then
     echo "Вы не ввели значение!"
     exit 1
 fi
 echo "Вы ввели: $NIBIRU_MONIKER"
-export NIBIRU_MONIKER
+export NIBIRU_MONIKER='$NIBIRU_MONIKER'
 
-read -p "Введите значение переменной: " NIBIRU_WALLET
+read -p "Введите значение переменной NIBIRU_WALLET: " NIBIRU_WALLET
 if [[ -z "$NIBIRU_WALLET" ]]; then
     echo "Вы не ввели значение!"
     exit 1
 fi
 echo "Вы ввели: $NIBIRU_WALLET"
-export NIBIRU_WALLET
+export NIBIRU_WALLET='$NIBIRU_WALLET'
