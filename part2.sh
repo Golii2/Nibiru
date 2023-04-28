0be47a6b4ad291c3b3865ce1df20ecd31a273260
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-29 00:53:25 +0400

    Create part2.sh

---
+++ part2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+nibid keys add $NIBIRU_WALLET --recover
+NIBIRU_ADDR=$(nibid keys show $NIBIRU_WALLET -a)
+echo 'export NIBIRU_ADDR='${NIBIRU_ADDR} >> $HOME/.bash_profile
+source $HOME/.bash_profile
+FAUCET_URL="https://faucet.itn-1.nibiru.fi/"
+NIBIRU_ADDR="..."
+curl -X POST -d '{"address": "'"$NIBIRU_ADDR"'", "coins": ["11000000unibi","100000000unusd","100000000uusdt"]}' $FAUCET_URL
+sleep 30
+nibid query bank balances $NIBIRU_ADDR
+sleep 5
+
+nibid tx staking create-validator \
+--amount 10000000unibi \
+--commission-max-change-rate "0.1" \
+--commission-max-rate "0.20" \
+--commission-rate "0.1" \
+--min-self-delegation "1" \
+--pubkey=$(nibid tendermint show-validator) \
+--moniker $NIBIRU_MONIKER \
+--chain-id nibiru-itn-1 \
+--from $NIBIRU_WALLET \
+--fees 5000unibi
+
+NIBIRU_VALOPER=$(nibid keys show $NIBIRU_ADDR --bech val -a)
+
+echo 'export NIBIRU_VALOPER='${NIBIRU_VALOPER} >> $HOME/.bash_profile
+source $HOME/.bash_profile
+nibid query staking validator $NIBIRU_VALOPER
