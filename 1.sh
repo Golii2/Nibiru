443bf300a2ee68e6e4a092e1133b5ccabc6a7b4a
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-28 18:52:24 +0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,18 +4,18 @@
 echo "Введите значение для переменной NIBIRU_MONIKER:"
 read NIBIRU_MONIKER
 
-# Присваиваем введенное значение переменной VARNAME
+# Присваиваем введенное значение переменной NIBIRU_MONIKER
 export NIBIRU_MONIKER="$NIBIRU_MONIKER"
 
 # Подтверждаем, что значение переменной успешно задано
-echo "Значение переменной VARNAME: $NIBIRU_MONIKER"
+echo "Значение переменной NIBIRU_MONIKER: $NIBIRU_MONIKER"
 
 # Запрашиваем у пользователя значение для переменной
 echo "Введите значение для переменной NIBIRU_WALLET:"
 read NIBIRU_WALLET
 
-# Присваиваем введенное значение переменной VARNAME
+# Присваиваем введенное значение переменной NIBIRU_WALLET
 export NIBIRU_WALLET="$NIBIRU_WALLET"
 
 # Подтверждаем, что значение переменной успешно задано
-echo "Значение переменной VARNAME: $NIBIRU_WALLET"
+echo "Значение переменной NIBIRU_WALLET: $NIBIRU_WALLET"
