b4cd9316a7a4f2f5da44293a390445aac3af30b8
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-28 15:52:34 +0400

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-read -p "Введите значение переменной NIBIRU_MONIKER: "
+read -p "Введите значение переменной NIBIRU_MONIKER: " NIBIRU_MONIKER
 echo "Переменная NIBIRU_MONIKER равна: $NIBIRU_MONIKER
 NIBIRU_CHAIN="nibiru-itn-1"
-read -p "Введите значение переменной NIBIRU_WALLET: " 
+read -p "Введите значение переменной NIBIRU_WALLET: " NIBIRU_WALLET
 echo "Переменная NIBIRU_WALLET равна: $NIBIRU_WALLET
 #1
 echo 'export NIBIRU_MONIKER='${NIBIRU_MONIKER} >> $HOME/.bash_profile
