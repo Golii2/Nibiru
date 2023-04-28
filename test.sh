5cf651f73858491bd73f680dbea62aa5137bab88
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-28 16:50:56 +0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#read -p "Введите значение переменной NIBIRU_MONIKER: " NIBIRU_MONIKER
+#echo "Переменная NIBIRU_MONIKER равна: $NIBIRU_MONIKER"
+
+echo "Введите значение переменной:"
+read NIBIRU_MONIKER
+echo "Вы ввели: $NIBIRU_MONIKER"
+export NIBIRU_MONIKER="$NIBIRU_MONIKER"
+
+#read -p "Введите значение переменной NIBIRU_WALLET: " NIBIRU_WALLET
+#echo "Переменная NIBIRU_WALLET равна: $NIBIRU_WALLET"
+
+echo "Введите значение переменной:"
+read NIBIRU_WALLET
+echo "Вы ввели: $NIBIRU_WALLET"
+export NIBIRU_WALLET="$NIBIRU_WALLET"
