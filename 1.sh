bde642aa281bd7d3a6486b858540c8a81b749d53
Golii2 <109893039+Golii2@users.noreply.github.com>
2023-04-28 18:20:45 +0400

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-read -p "Введите значение переменной NIBIRU_MONIKER: " NIBIRU_MONIKER
+read -p "Введите значение переменной NIBIRU_MONIKER: " $NIBIRU_MONIKER=
 while [[ -z $NIBIRU_MONIKER ]]; do
-    read -p "Введите значение: " NIBIRU_MONIKER
+    read -p "Введите значение: " $NIBIRU_MONIKER
 done
 echo "Вы ввели: $NIBIRU_MONIKER"
 export NIBIRU_MONIKER='$NIBIRU_MONIKER'
 
-read -p "Введите значение переменной NIBIRU_WALLET: " NIBIRU_WALLET
+read -p "Введите значение переменной NIBIRU_WALLET: " $NIBIRU_WALLET=
 while [[ -z $NIBIRU_WALLET ]]; do
-    read -p "Введите значение: " IBIRU_WALLET
+    read -p "Введите значение: " $NIBIRU_WALLET
 done
 echo "Вы ввели: $NIBIRU_WALLET"
 export NIBIRU_WALLET='$NIBIRU_WALLET'
